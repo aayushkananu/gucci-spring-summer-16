41cda9b357ec852568491a4adce66c4eeab16b33
aayushkananu <112506495+aayushkananu@users.noreply.github.com>
2023-08-30 21:24:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,21 @@
 #### COLLECTION ####
-valentino_collection_name = " VALENTINO HAUTE COUTURE SS23"
+gucci_collection_name = " GUCCI SS16"
 
 ## DEFINE THE TRENDS
-valentino_trends = {
-  #material and texture
-  'sheer': 0,
-  'plaid':0, 
-  'ruffles':0,
-  'stockings':0,
-  'stripes':0, 
-  'leather':0,
-  'fur':0,
-
-  #items 
-  'blazer':0, 
-  'v_neck':0, 
-  'hat':0,
-  'costume_jewelry':0, 
-  'strapless':0, 
-  'asymmetric':0,
-  'mini_dress':0,
-  'button_down':0, 
-  'shorts':0,
-  'hot_shorts':0, 
-  'maxi_dress':0,
-  'maxi_skirt':0,
-  'jeans':0,
-  'crossbody_bag':0, 
-  'clutch':0, 
-  'gloves':0,
-  'tie':0,
-  'bow':0,
-  'maxi':0,
-  'parachute pants':0,
-  'cape':0,
-  'goggles':0,
-  'button_up':0,
-  'fishnet':0,
-  'long_coat':0,
-
-  'valentino_logo':0, 
-
-  #colors
-  'red':0, 
-  'purple':0,
-  'black':0, 
-  'blue':0, 
-  'yellow':0, 
-  'pink':0, 
-  'orange':0, 
-  'white':0, 
-  'navy':0, 
-  'green':0, 
-  'shimmer':0, 
-  'grey':0,
-
-  #shoes
-  'ankle_straps':0, 
-  'open_toe':0, 
-  'ballet_flats':0,
-  'platform_heels':0,
-  'boots':0,
-  'close_toed':0,
-  }      
+gucci_trends = {
+  'logo_belt': 0
+}  
 
 ## DEFINE THE LOOKS
-valentino_looks = [
-['tie','pink','white','black','blazer','button_up','bow', 'ballet_flats' ], #look1
+gucci_looks = [
+  [], 
+  [], 
+  [], 
+  [], 
+  [1],
+  [], 
+  [], 
+  
 ['black','close_toed','goggles','blazer'],
 ['black','polka_dots','stockings' ,'ankle_strap'],
 ['pink','black','white','googles','cape','fishnet','polka_dots','stockings','platform_heels'],
@@ -174,6 +123,7 @@ for trend, var in trends.items():
     trends.update({trend:count})
 
 ## GET PERCENTAGES
+
 trend_percentages = {}
 for trend, var in trends.items():
     percentage = var / total * 100
